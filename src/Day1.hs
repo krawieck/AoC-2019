95ea2fdd2611461fe6d19ffaf7d47e42829665f2
krawieck <fil.krawczyk@gmail.com>
2019-12-21 00:56:03 +0100

    Day 1 done ... better late than never

---
+++ src/Day1.hs
@@ -0,0 +1,18 @@
+module Day1 where
+
+import           System.IO
+import           Data.List.Split
+import           Flow
+
+day1 = do
+  contents <- readFile "src/input/day1.txt"
+  contents
+    |> splitOn "\n"
+    |> init
+    |> map (\x -> (read x :: Int))
+    |> map calculateFuel
+    |> sum
+    |> print
+
+
+calculateFuel x = (x `div` 3) - 2
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,9 @@
 module Main where
 
+import           Day1
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStr "Day 1: "
+  day1
+
