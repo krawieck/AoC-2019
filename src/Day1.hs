2af36eb0fc6f978660613f3e6ed44059c874671e
krawieck <fil.krawczyk@gmail.com>
2019-12-21 15:26:25 +0100

    Done day 1 part 2

--- src/Day1.hs
+++ src/Day1.hs
@@ -4,7 +4,7 @@ import           System.IO
 import           Data.List.Split
 import           Flow
 
-day1 = do
+part1 = do
   contents <- readFile "src/input/day1.txt"
   contents
     |> splitOn "\n"
@@ -14,5 +14,19 @@ day1 = do
     |> sum
     |> print
 
+part2 = do
+  contents <- readFile "src/input/day1.txt"
+  contents
+    |> splitOn "\n"
+    |> init
+    |> map (\x -> read x :: Int)
+    |> map calculateFuelRecursive
+    |> sum
+    |> print
 
 calculateFuel x = (x `div` 3) - 2
+
+
+calculateFuelRecursive :: Int -> Int
+calculateFuelRecursive x =
+  let z = (x `div` 3) - 2 in if z > 0 then z + calculateFuelRecursive z else 0
--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,10 @@ import           Day1
 
 main :: IO ()
 main = do
-  putStr "Day 1: "
-  day1
+  putStrLn "Day 1: "
+  putStr "  Part 1: "
+  Day1.part1
+  putStr "  Part 2: "
+  Day1.part2
+
 
